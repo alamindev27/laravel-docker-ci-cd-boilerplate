5fdef054efe47a2e17d3db34cac4bb59d6980321
MD Al-Amin <alamindev27@gmail.com>
2026-09-05 00:39:24 +0600

    feat: add docker automation scripts, UserSeeder, and update user database schema

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->default('uploads/avatar.png');
+            $table->enum('role', ['user', 'admin'])->default('user');
+            $table->enum('status', ['active', 'deactive'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
